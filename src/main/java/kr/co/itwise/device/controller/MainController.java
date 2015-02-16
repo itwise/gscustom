a5649fa730b50a54e068aafe3c6710665263368d
kimdongjun <djkim@itwise.co.kr>
2015-02-16 11:34:02 +0900

    redirect 관련 수정

--- src/main/java/kr/co/itwise/device/controller/MainController.java
+++ src/main/java/kr/co/itwise/device/controller/MainController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.mobile.device.Device;
 import org.springframework.mobile.device.DeviceUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,12 +31,13 @@ public class MainController {
 	
 	@RequestMapping(value="/dummy", method = RequestMethod.POST)
 	public ModelAndView getDeviceInfo(@RequestParam("deviceWidth") int deviceWidth
+			, @RequestParam("redirect") String redirect
 			, @RequestParam("deviceHeight") int deviceHeight
 			, @RequestParam("deviceRatio") int deviceRatio
 			, HttpServletRequest request
 			, HttpServletResponse response){
-		ModelAndView mav = new ModelAndView("blank");
-		
+		//ModelAndView mav = new ModelAndView("blank");
+		System.out.println("redirect : " + redirect);
 		Device device = DeviceUtils.getCurrentDevice(request);
 		Cookie cookie;
 		if(device.isNormal()){
@@ -48,7 +48,12 @@ public class MainController {
 		cookie.setMaxAge(60*60*24*7);
 		cookie.setPath("/");
 		response.addCookie(cookie);
-		return mav;
+		
+		if("".equals(redirect)){
+			return new ModelAndView("redirect:/main");
+		}else{
+			return new ModelAndView("redirect:" + redirect);
+		}
 	}
 	
 	@RequestMapping(value="/test")
